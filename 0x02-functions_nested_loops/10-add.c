fd41aae0959deb4d71dcb43eacc8edd5cf792e49
Ecurb23 <bruceyuya04@gmail.com>
2023-09-13 13:44:31 -0700

    mandatory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+# include "main.h"
+/**
+ * add - adds two numbers
+ * @n1 - the first number
+ * @n2 - the second number
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * jack_bauer - hkjhkjh
- *
+ * jack_bauer - Prints every minute of the day
+ * Return:
  */
 void jack_bauer(void)
 {
@@ -11,18 +11,20 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c <= 5; c++)
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
-				for (d = 0; d <= 9; d++)
+				for (c = 0; c <= 5; c++)
 				{
-					if (a >= 2 && b >= 4)
-						break;
-					_putchar(a + 48);
-					_putchar(b + 48);
-					_putchar(58);
-					_putchar(c + 48);
-					_putchar(d + 48);
-				}	_putchar('\n');
+					for (d = 0; d <= 9; d++)
+					{
+						_putchar(a + 48);
+						_putchar(b + 48);
+						_putchar(58);
+						_putchar(c + 48);
+						_putchar(d + 48);
+						_putchar('\n');
+					}
+				}
 			}
 		}
 	}
