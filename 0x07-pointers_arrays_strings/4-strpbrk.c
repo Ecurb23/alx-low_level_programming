4d4281712304289cd7e2862e826dccaaea9d8882
Ecurb23 <bruceyuya04@gmail.com>
2023-09-25 22:01:51 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,14 +11,14 @@ char *_strpbrk(char *s, char *accept)
 {
 	int index;
 
-    while (*s)
-    {
-        for (index = 0; accept[index]; index++)
-        {
-            if (*s == accept[index])
-                    return (s);     
-        }   
-        s++;
-    }
-	return (NULL);
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+		if (*s == accept[index])
+		return (s);
+		}
+		s++;
+	}
+		return (NULL);
 }
